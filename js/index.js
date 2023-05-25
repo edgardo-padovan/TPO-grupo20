cd247a589985ebf665258ec42d7b48977431de85
edgardo-padovan <edgardopadovan@gmail.com>
2023-05-25 09:52:40 -0300

    Web Terminada

--- js/index.js
+++ js/index.js
@@ -1,8 +1,71 @@
-HEAD
-// Dropdown Menu
-c267a0b5b06d21509a37d753ef93973199500795
-const toggleHeader = document.querySelector("#toggle-header")
-const dropDownMenu = document.querySelector(".nav-menu")
-toggleHeader.onclick = function() {
-    dropDownMenu.classList.toggle("open")
+//Carousel
+const carousel = document.querySelector('.carousel');
+const flechasIzquierda = document.querySelector('.fa-angles-left');
+const flechasDerecha = document.querySelector('.fa-angles-right');
+const puntos = document.querySelectorAll('.punto');
+
+let currentIndex = 0;
+const totalBanners = 3;
+let timer 
+
+function actualizarPuntos() {
+  puntos.forEach(punto => punto.classList.remove('activo'));
+  puntos[currentIndex].classList.add('activo');
 }
+
+function moverCarousel() {
+  const movimiento = currentIndex * -(100 / totalBanners);
+  carousel.style.transform = `translateX(${movimiento}%)`;
+}
+
+function iniciarTimer() {
+  timer = setInterval(() => {
+    if (currentIndex === totalBanners - 1) {
+        currentIndex = 0;
+      } else {
+        currentIndex = currentIndex + 1;
+      }      
+    moverCarousel();
+    actualizarPuntos();
+  }, 6500);
+}
+
+function detenerTimer() {
+  clearInterval(timer);
+}
+
+flechasIzquierda.addEventListener('click', () => {
+   if (currentIndex === 0) {
+        currentIndex = totalBanners - 1;
+      } else {
+        currentIndex = currentIndex - 1;
+      }      
+  moverCarousel();
+  actualizarPuntos();
+  detenerTimer();
+  iniciarTimer();
+});
+
+flechasDerecha.addEventListener('click', () => {
+    if (currentIndex === totalBanners - 1) {
+        currentIndex = 0;
+      } else {
+        currentIndex = currentIndex + 1;
+      }   
+  moverCarousel();
+  actualizarPuntos();
+  detenerTimer();
+  iniciarTimer();
+});
+
+puntos.forEach((punto, index) => {
+  punto.addEventListener('click', () => {
+    currentIndex = index;
+    moverCarousel();
+    actualizarPuntos();
+    detenerTimer();
+    iniciarTimer();
+  });
+});
+
+iniciarTimer();
